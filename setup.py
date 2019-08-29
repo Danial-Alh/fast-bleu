e428032331926a0513dc2dea34804554592a52cd
danial alihosseini <danial.alihosseini@gmail.com>
2019-08-29 16:11:06 +0430

    version changed to 0.0.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
 include_dirs = ['fast_bleu/cpp_sources/headers/']
 setup = setuptools.setup(
     name='FastBLEU',
-    version="0.0.3",
+    version="0.0.4",
     author="Danial Alihosseini",
     author_email="danial.alihosseini@gmail.com",
     description="A fast multithreaded C++ implementation of nltk BLEU.",
