1bfa9b0e1b3e9b5721bd9f85975d777ec6c78a59
Danial Alihosseini <danial.alihosseini@gmail.com>
2020-08-12 16:28:16 +0430

    adding `gcc` and `g++` compiler path option.

---
+++ fast_bleu/__test.py
@@ -0,0 +1,27 @@
+def test():
+    from fast_bleu import BLEU, SelfBLEU
+
+    ref1 = ['It', 'is', 'a', 'guide', 'to', 'action', 'that',
+            'ensures', 'that', 'the', 'military', 'will', 'forever',
+            'heed', 'Party', 'commands']
+    ref2 = ['It', 'is', 'the', 'guiding', 'principle', 'which',
+            'guarantees', 'the', 'military', 'forces', 'always',
+            'being', 'under', 'the', 'command', 'of', 'the', 'Party']
+    ref3 = ['It', 'is', 'the', 'practical', 'guide', 'for', 'the',
+            'army', 'always', 'to', 'heed', 'the', 'directions',
+            'of', 'the', 'party']
+    hyp1 = ['It', 'is', 'a', 'guide', 'to', 'action', 'which',
+            'ensures', 'that', 'the', 'military', 'always',
+            'obeys', 'the', 'commands', 'of', 'the', 'party']
+    hyp2 = ['he', 'read', 'the', 'book', 'because', 'he', 'was',
+            'interested', 'in', 'world', 'history']
+
+    list_of_references = [ref1, ref2, ref3]
+    hypotheses = [hyp1, hyp2]
+
+    weights = {'bigram': (1/2., 1/2.), 'trigram': (1/3., 1/3., 1/3.)}
+    bleu = BLEU(list_of_references, weights)
+    print(bleu.get_score(hypotheses))
+
+    self_bleu = SelfBLEU(list_of_references, weights)
+    print(self_bleu.get_score())
--- setup.py
+++ setup.py
@@ -1,8 +1,37 @@
+import os
+
 from glob import glob
 
 import setuptools
 from setuptools.command.build_ext import build_ext
 from setuptools.extension import Extension
+from setuptools.command.install import install
+
+
+class InstallCommand(install):
+    user_options = install.user_options + [
+        ('CC=', None, '<gcc binary path>'),
+        ('CXX=', None, '<g++ binary path>'),
+    ]
+
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.CC = None
+        self.CXX = None
+
+    def finalize_options(self):
+        print("Value of CC is", self.CC)
+        print("Value of CXX is", self.CXX)
+        install.finalize_options(self)
+
+    def run(self):
+        if self.CC is not None:
+            os.environ["CC"] = self.CC
+            print("CC changed to {}".format(self.CC))
+        if self.CXX is not None:
+            os.environ["CXX"] = self.CXX
+            print("CXX changed to {}".format(self.CXX))
+        install.run(self)
 
 
 class BuildExtWithoutPlatformSuffix(build_ext):
@@ -14,7 +43,6 @@ class BuildExtWithoutPlatformSuffix(build_ext):
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-include_dirs = ['fast_bleu/cpp_sources/headers/']
 setup = setuptools.setup(
     name='fast-bleu',
     version="0.0.85",
@@ -30,9 +58,9 @@ setup = setuptools.setup(
             sources=glob('fast_bleu/cpp_sources/sources/*.cpp'),
             extra_compile_args=['-fopenmp', '-std=c++11'],
             extra_link_args=['-fopenmp', '-std=c++11'],
-            include_dirs=include_dirs,
+            include_dirs=['fast_bleu/cpp_sources/headers/'],
         ), ],
-    cmdclass={'build_ext': BuildExtWithoutPlatformSuffix},
+    cmdclass={'install': InstallCommand, 'build_ext': BuildExtWithoutPlatformSuffix},
     packages=['fast_bleu'],
     classifiers=[
         "Programming Language :: Python :: 3",
