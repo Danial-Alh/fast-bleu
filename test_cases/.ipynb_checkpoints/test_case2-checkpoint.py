503b54b83be53d6d37e47f2ca2e93d91a439db61
Danial-Alh <danial.alihosseini@gmail.com>
2021-09-01 12:09:52 +0430

    delete unwanted files.

--- test_cases/.ipynb_checkpoints/test_case2-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from fast_bleu import BLEU
-
-reference_corpus = [[str(i) + str(j) for i in range(100)] for j in range(70000)]
-weight = {'4': (.25, .25, .25, .25)}
-BLEU(reference_corpus, weight, verbose=True)
